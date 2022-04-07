5199195af8b30c88d53917677298835b99fd84cd
Adebayo Solomon <adebayo.solomon17@gmail.com>
2022-04-07 05:19:43 -0700

    another correction for 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,11 @@
 1 #include <stdio.h>
 2
 3 /**
-4 
- * main - prints the phrase "Programming is like building a multilingual puzzl
+4 * main - prints the phrase "Programming is like building a multilingual puzzl
 5 * Return: 0 if exited properly, non-zero otherwise
 6 */
 7 int main(void)
 8 {
-9 
-puts("\"Programming is like building a multilingual puzzle");
+9 puts("\"Programming is like building a multilingual puzzle");
 10 return (0);
 11 }
